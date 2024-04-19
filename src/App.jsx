3e9f0ae22232248f44c08629b8dbf4862ef16cb1
KANCHAN KUMARI <kanchan1070bit@gmail.com>
2024-04-19 14:41:44 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <img
+          className="barcode-img"
+          src="./src/assets/image-qr-code.png"
+          alt="Bar Code"
+        ></img>
+        <p className="barcode-para1">
+          Improve your front-end skills by building projects
+        </p>
+        <p className="barcode-para2">
+          Scan the QR code to visit Frontend Mentor and take coding skills to
+          the next level
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
